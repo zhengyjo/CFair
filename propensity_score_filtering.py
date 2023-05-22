7eda37af914402cb39fb2dfe7836b0c8f2a04d9e
zhengyjo <zhengyjo@brandeis.edu>
2023-05-22 16:15:16 -0400

    Update and rename propensity_score_delta_selection.py to propensity_score_filtering.py

--- propensity_score_filtering.py
+++ propensity_score_filtering.py
@@ -22,27 +22,6 @@ from scipy.spatial import distance
 from sklearn.linear_model import LogisticRegression
 
 
-def filter_with_delta_threshold(distance_psm_all, all_pair_distance_lst, delta):
-    """
-    First stage of counter-part filtering with given delta
-    Parameters:
-    -------
-    distance_psm_all: the data frame of minority group
-    delta: The p-value threshold for null distribution of all the pair-wise difference among pairs with one candidate
-    from major group and the other one from minority group
-
-    Return :
-    -------
-    list.  The indices of qualified neighbor for each minority candidate.
-    """
-    threshold = np.quantile(all_pair_distance_lst, delta)
-    candidate_index_lst = []
-    for i in range(len(distance_psm_all)):
-        temp_lst = np.where(np.array(distance_psm_all[i]) < threshold)
-        candidate_index_lst.append(temp_lst)
-    return candidate_index_lst
-
-
 # create the parser object
 parser = argparse.ArgumentParser(description='Example argparse script')
 parser.add_argument('input_file', help='The file including all the pair-wise distance')
@@ -55,7 +34,8 @@ all_pair_distance = args.input_file_2
 p_value = float(args.delta)
 name = args.name
 
+# Pass the original candidates list and the p-value threshold to filter out unqualified candidates 
 candidate_index_lst = filter_with_delta_threshold(all_pair_distance, all_pair_distance_lst, p_value)
 
-with open(name, "wb") as fp:  # Pickling
+with open(name, "wb") as fp:  # Pickling the results
     pickle.dump(candidate_index_lst, fp)
